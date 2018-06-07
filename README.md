214aaf6d54f3ddd50ec834d0848267f26d2c22cf
Ben Eustace <awks.ben@gmail.com>
2018-06-07 16:24:51 +0100

    Adding kms encryption option to sqs

--- README.md
+++ README.md
@@ -32,21 +32,46 @@ Module usage:
 
 ## Inputs
 
-| Name | Description | Default | Required |
-|------|-------------|:-----:|:-----:|
-| content_based_deduplication | Enables content-based deduplication for FIFO queues | `false` | no |
-| delay_seconds | The time in seconds that the delivery of all messages in the queue will be delayed. An integer from 0 to 900 (15 minutes) | `0` | no |
-| environment | The environment the SQS is running in i.e. dev, prod etc | - | yes |
-| fifo_queue | Boolean designating a FIFO queue | `false` | no |
-| iam_user_policy_name | The policy name of attached to the user | - | yes |
-| kms_alias | KMS key alias name for SQS | `` | no |
-| max_message_size | The limit of how many bytes a message can contain before Amazon SQS rejects it. An integer from 1024 bytes (1 KiB) up to 262144 bytes (256 KiB) | `262144` | no |
-| message_retention_seconds | The number of seconds Amazon SQS retains a message. Integer representing seconds, from 60 (1 minute) to 1209600 (14 days) | `345600` | no |
-| name | A descriptive name for the SQS instance | - | yes |
-| number_of_users | The number of user to generate credentials for | `1` | no |
-| policy | The JSON policy for the SQS queue | `` | no |
-| receive_wait_time_seconds | The time for which a ReceiveMessage call will wait for a message to arrive (long polling) before returning. An integer from 0 to 20 (seconds) | `0` | no |
-| sqs_iam_user | The name of the iam user assigned to the created sqs bucket | - | yes |
-| tags | A map of tags to add to all resources | `<map>` | no |
-| visibility_timeout_seconds | The visibility timeout for the queue. An integer from 0 to 43200 (12 hours) | `30` | no |
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| content_based_deduplication | Enables content-based deduplication for FIFO queues | string | `false` | no |
+| delay_seconds | The time in seconds that the delivery of all messages in the queue will be delayed. An integer from 0 to 900 (15 minutes) | string | `0` | no |
+| environment | The environment the SQS is running in i.e. dev, prod etc | string | - | yes |
+| fifo_queue | Boolean designating a FIFO queue | string | `false` | no |
+| iam_user_policy_name | The policy name of attached to the user | string | - | yes |
+| kms_alias | KMS key alias name for SQS | string | `` | no |
+| kms_key | KMS key id name for SQS | string | `` | no |
+| max_message_size | The limit of how many bytes a message can contain before Amazon SQS rejects it. An integer from 1024 bytes (1 KiB) up to 262144 bytes (256 KiB) | string | `262144` | no |
+| max_receive_count | The max receive count for a queue before Amazon SQS moves the message to a dead-letter queue | string | `10` | no |
+| message_retention_seconds | The number of seconds Amazon SQS retains a message. Integer representing seconds, from 60 (1 minute) to 1209600 (14 days) | string | `345600` | no |
+| name | A descriptive name for the SQS instance | string | - | yes |
+| number_of_users | The number of user to generate credentials for | string | `1` | no |
+| policy | The JSON policy for the SQS queue | string | `` | no |
+| receive_wait_time_seconds | The time for which a ReceiveMessage call will wait for a message to arrive (long polling) before returning. An integer from 0 to 20 (seconds) | string | `0` | no |
+| redrive_arn | AWS ARN for the SQS redirect queue | string | `` | no |
+| sqs_iam_user | The name of the iam user assigned to the created sqs bucket | string | - | yes |
+| tags | A map of tags to add to all resources | string | `<map>` | no |
+| visibility_timeout_seconds | The visibility timeout for the queue. An integer from 0 to 43200 (12 hours) | string | `30` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| kms_key | The kms key generated for the sqs resource |
+| sqs_arn | The SQS Queue ARN we just created |
+| sqs_arn_kms | The SQS KMS Queue ARN we just created |
+| sqs_arn_kms_and_no_policy | The SQS KMS Queue ARN we just created |
+| sqs_arn_kms_and_redrive | The SQS KMS Queue ARN we just created |
+| sqs_arn_kms_and_redrive_and_no_policy | The SQS KMS Queue ARN we just created |
+| sqs_arn_no_policy | The SQS KMS Queue ARN we just created |
+| sqs_arn_redrive | The SQS KMS Queue ARN we just created |
+| sqs_arn_redrive_and_no_policy | The SQS KMS Queue ARN we just created |
+| sqs_id | The SQS Queue ID we just created |
+| sqs_id_kms | The SQS KMS Queue ID we just created |
+| sqs_id_kms_and_no_policy | The SQS KMS Queue ID we just created |
+| sqs_id_kms_and_redrive | The SQS KMS Queue ID we just created |
+| sqs_id_kms_and_redrive_and_no_policy | The SQS KMS Queue ID we just created |
+| sqs_id_no_policy | The SQS KMS Queue ID we just created |
+| sqs_id_redrive | The SQS KMS Queue ID we just created |
+| sqs_id_redrive_and_no_policy | The SQS KMS Queue ID we just created |
 
--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 /**
 * Module usage:
-* 
+*
 *      module "sqs" {
 *        source               = "git::https://github.com/UKHomeOffice/acp-tf-sqs?ref=master"
 *        name                 = "new-sqs"
 *        environment          = "env"
 *        sqs_iam_user         = "new-sqs-user"
 *        iam_user_policy_name = "new-sqs-policy"
-* 
+*
 *        policy = <<POLICY
 *      {
 *        "Version": "2012-10-17",
@@ -36,7 +36,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 resource "aws_kms_key" "sqs_kms_key" {
-  count = "${length(var.kms_alias) == 0 ? 0 : 1}"
+  count = "${length(var.kms_alias) != 0 && ( length(var.policy) == 0 || length(var.redrive_arn) == 0 ) ? 1 : 0 }"
 
   deletion_window_in_days = 7
   is_enabled              = "true"
@@ -44,30 +44,31 @@ resource "aws_kms_key" "sqs_kms_key" {
 }
 
 resource "aws_kms_alias" "s3_bucket_kms_alias" {
-  count = "${length(var.kms_alias) == 0 ? 0 : 1}"
+  count = "${length(var.kms_alias) != 0 && ( length(var.policy) == 0 || length(var.redrive_arn) == 0 ) ? 1 : 0 }"
 
   name          = "alias/${var.kms_alias}"
   target_key_id = "${aws_kms_key.sqs_kms_key.key_id}"
 }
 
 resource "aws_sqs_queue" "queue" {
-  count = "${length(var.kms_alias) == 0 ? 1 : 0}"
+  count = "${length(var.kms_alias) == 0 && length(var.redrive_arn) == 0 && length(var.policy) != 0 ? 1 : 0}"
   name  = "${var.name}"
 
-  visibility_timeout_seconds  = "${var.visibility_timeout_seconds}"
-  message_retention_seconds   = "${var.message_retention_seconds}"
-  max_message_size            = "${var.max_message_size}"
-  policy                      = "${var.policy}"
-  delay_seconds               = "${var.delay_seconds}"
-  receive_wait_time_seconds   = "${var.receive_wait_time_seconds}"
-  fifo_queue                  = "${var.fifo_queue}"
-  content_based_deduplication = "${var.content_based_deduplication}"
+  visibility_timeout_seconds        = "${var.visibility_timeout_seconds}"
+  message_retention_seconds         = "${var.message_retention_seconds}"
+  max_message_size                  = "${var.max_message_size}"
+  policy                            = "${var.policy}"
+  delay_seconds                     = "${var.delay_seconds}"
+  receive_wait_time_seconds         = "${var.receive_wait_time_seconds}"
+  fifo_queue                        = "${var.fifo_queue}"
+  content_based_deduplication       = "${var.content_based_deduplication}"
+  kms_data_key_reuse_period_seconds = 300
 
   tags = "${merge(var.tags, map("Name", format("%s-%s", var.environment, var.name)), map("Env", var.environment), map("KubernetesCluster", var.environment))}"
 }
 
 resource "aws_sqs_queue" "queue_with_kms" {
-  count = "${length(var.kms_alias) == 0 ? 0 : 1}"
+  count = "${length(var.kms_alias) != 0 && length(var.redrive_arn) == 0 && length(var.policy) != 0 ? 1 : 0}"
   name  = "${var.name}"
 
   visibility_timeout_seconds        = "${var.visibility_timeout_seconds}"
@@ -84,38 +85,159 @@ resource "aws_sqs_queue" "queue_with_kms" {
   tags = "${merge(var.tags, map("Name", format("%s-%s", var.environment, var.name)), map("Env", var.environment), map("KubernetesCluster", var.environment))}"
 }
 
+resource "aws_sqs_queue" "queue_with_no_policy" {
+  count = "${length(var.kms_alias) == 0 && length(var.redrive_arn) == 0 && length(var.policy) == 0 ? 1 : 0}"
+  name  = "${var.name}"
+
+  visibility_timeout_seconds        = "${var.visibility_timeout_seconds}"
+  message_retention_seconds         = "${var.message_retention_seconds}"
+  max_message_size                  = "${var.max_message_size}"
+  delay_seconds                     = "${var.delay_seconds}"
+  receive_wait_time_seconds         = "${var.receive_wait_time_seconds}"
+  fifo_queue                        = "${var.fifo_queue}"
+  content_based_deduplication       = "${var.content_based_deduplication}"
+  kms_data_key_reuse_period_seconds = 300
+
+  tags = "${merge(var.tags, map("Name", format("%s-%s", var.environment, var.name)), map("Env", var.environment), map("KubernetesCluster", var.environment))}"
+}
+
+resource "aws_sqs_queue" "queue_with_kms_and_no_policy" {
+  count = "${length(var.kms_alias) != 0 && length(var.redrive_arn) == 0 && length(var.policy) == 0 ? 1 : 0}"
+  name  = "${var.name}"
+
+  visibility_timeout_seconds        = "${var.visibility_timeout_seconds}"
+  message_retention_seconds         = "${var.message_retention_seconds}"
+  max_message_size                  = "${var.max_message_size}"
+  delay_seconds                     = "${var.delay_seconds}"
+  receive_wait_time_seconds         = "${var.receive_wait_time_seconds}"
+  fifo_queue                        = "${var.fifo_queue}"
+  content_based_deduplication       = "${var.content_based_deduplication}"
+  kms_master_key_id                 = "${aws_kms_key.sqs_kms_key.key_id}"
+  kms_data_key_reuse_period_seconds = 300
+
+  tags = "${merge(var.tags, map("Name", format("%s-%s", var.environment, var.name)), map("Env", var.environment), map("KubernetesCluster", var.environment))}"
+}
+
+resource "aws_sqs_queue" "queue_with_redrive" {
+  count = "${length(var.kms_alias) == 0 && length(var.redrive_arn) != 0 && length(var.policy) != 0 ? 1 : 0}"
+  name  = "${var.name}"
+
+  visibility_timeout_seconds        = "${var.visibility_timeout_seconds}"
+  message_retention_seconds         = "${var.message_retention_seconds}"
+  max_message_size                  = "${var.max_message_size}"
+  policy                            = "${var.policy}"
+  delay_seconds                     = "${var.delay_seconds}"
+  receive_wait_time_seconds         = "${var.receive_wait_time_seconds}"
+  fifo_queue                        = "${var.fifo_queue}"
+  content_based_deduplication       = "${var.content_based_deduplication}"
+  kms_data_key_reuse_period_seconds = 300
+  redrive_policy                    = "{\"deadLetterTargetArn\":\"${var.redrive_arn}\",\"maxReceiveCount\":${var.max_receive_count}}"
+
+  tags = "${merge(var.tags, map("Name", format("%s-%s", var.environment, var.name)), map("Env", var.environment), map("KubernetesCluster", var.environment))}"
+}
+
+resource "aws_sqs_queue" "queue_with_kms_and_redrive" {
+  count = "${length(var.kms_alias) != 0 && length(var.redrive_arn) != 0 && length(var.policy) != 0 ? 1 : 0}"
+  name  = "${var.name}"
+
+  visibility_timeout_seconds        = "${var.visibility_timeout_seconds}"
+  message_retention_seconds         = "${var.message_retention_seconds}"
+  max_message_size                  = "${var.max_message_size}"
+  policy                            = "${var.policy}"
+  delay_seconds                     = "${var.delay_seconds}"
+  receive_wait_time_seconds         = "${var.receive_wait_time_seconds}"
+  fifo_queue                        = "${var.fifo_queue}"
+  content_based_deduplication       = "${var.content_based_deduplication}"
+  kms_master_key_id                 = "${var.kms_key}"
+  kms_data_key_reuse_period_seconds = 300
+  redrive_policy                    = "{\"deadLetterTargetArn\":\"${var.redrive_arn}\",\"maxReceiveCount\":${var.max_receive_count}}"
+
+  tags = "${merge(var.tags, map("Name", format("%s-%s", var.environment, var.name)), map("Env", var.environment), map("KubernetesCluster", var.environment))}"
+}
+
+resource "aws_sqs_queue" "queue_with_redrive_and_no_policy" {
+  count = "${length(var.kms_alias) == 0 && length(var.redrive_arn) != 0 && length(var.policy) == 0 ? 1 : 0}"
+  name  = "${var.name}"
+
+  visibility_timeout_seconds        = "${var.visibility_timeout_seconds}"
+  message_retention_seconds         = "${var.message_retention_seconds}"
+  max_message_size                  = "${var.max_message_size}"
+  delay_seconds                     = "${var.delay_seconds}"
+  receive_wait_time_seconds         = "${var.receive_wait_time_seconds}"
+  fifo_queue                        = "${var.fifo_queue}"
+  content_based_deduplication       = "${var.content_based_deduplication}"
+  kms_data_key_reuse_period_seconds = 300
+  redrive_policy                    = "{\"deadLetterTargetArn\":\"${var.redrive_arn}\",\"maxReceiveCount\":${var.max_receive_count}}"
+
+  tags = "${merge(var.tags, map("Name", format("%s-%s", var.environment, var.name)), map("Env", var.environment), map("KubernetesCluster", var.environment))}"
+}
+
+resource "aws_sqs_queue" "queue_with_kms_and_redrive_and_no_policy" {
+  count = "${length(var.kms_alias) != 0 && length(var.redrive_arn) != 0 && length(var.policy) == 0 ? 1 : 0}"
+  name  = "${var.name}"
+
+  visibility_timeout_seconds        = "${var.visibility_timeout_seconds}"
+  message_retention_seconds         = "${var.message_retention_seconds}"
+  max_message_size                  = "${var.max_message_size}"
+  delay_seconds                     = "${var.delay_seconds}"
+  receive_wait_time_seconds         = "${var.receive_wait_time_seconds}"
+  fifo_queue                        = "${var.fifo_queue}"
+  content_based_deduplication       = "${var.content_based_deduplication}"
+  kms_master_key_id                 = "${aws_kms_key.sqs_kms_key.key_id}"
+  kms_data_key_reuse_period_seconds = 300
+  redrive_policy                    = "{\"deadLetterTargetArn\":\"${var.redrive_arn}\",\"maxReceiveCount\":${var.max_receive_count}}"
+
+  tags = "${merge(var.tags, map("Name", format("%s-%s", var.environment, var.name)), map("Env", var.environment), map("KubernetesCluster", var.environment))}"
+}
+
 resource "aws_iam_user" "sqs_iam_user" {
-  count = "${length(var.kms_alias) == 0 ? var.number_of_users : 0}"
+  count = "${length(var.policy) != 0 && length(var.kms_alias) == 0 ? var.number_of_users : 0}"
+
+  name = "${var.sqs_iam_user}${var.number_of_users != 1 ? "-${count.index}" : "" }"
+  path = "/"
+}
+
+resource "aws_iam_user" "sqs_with_kms_iam_user" {
+  count = "${length(var.policy) != 0 && length(var.kms_alias) != 0 ? var.number_of_users : 0}"
 
   name = "${var.sqs_iam_user}${var.number_of_users != 1 ? "-${count.index}" : "" }"
   path = "/"
 }
 
 resource "aws_iam_user_policy" "sqs_user_policy" {
-  count = "${length(var.kms_alias) == 0 ? var.number_of_users : 0}"
+  count = "${length(var.kms_alias) == 0 && length(var.redrive_arn) == 0 && length(var.policy) != 0 ? var.number_of_users : 0}"
 
   name   = "${var.iam_user_policy_name}SQSPolicy"
   user   = "${element(aws_iam_user.sqs_iam_user.*.name, count.index)}"
   policy = "${data.aws_iam_policy_document.sqs_policy_document.json}"
 }
 
-resource "aws_iam_user" "sqs_with_kms_iam_user" {
-  count = "${length(var.kms_alias) == 0 ? 0 : var.number_of_users }"
-
-  name = "${var.sqs_iam_user}${var.number_of_users != 1 ? "-${count.index}" : "" }"
-  path = "/"
-}
-
 resource "aws_iam_user_policy" "sqs_with_kms_user_policy" {
-  count = "${length(var.kms_alias) == 0 ? 0 : var.number_of_users }"
+  count = "${length(var.kms_alias) != 0 && length(var.redrive_arn) == 0 && length(var.policy) != 0 ? var.number_of_users : 0 }"
 
   name   = "${var.iam_user_policy_name}SQSPolicy"
   user   = "${element(aws_iam_user.sqs_with_kms_iam_user.*.name, count.index)}"
   policy = "${data.aws_iam_policy_document.sqs_with_kms_policy_document.json}"
 }
 
+resource "aws_iam_user_policy" "sqs_with_redrive_user_policy" {
+  count = "${length(var.kms_alias) == 0 && length(var.redrive_arn) != 0 && length(var.policy) != 0 ? var.number_of_users : 0}"
+
+  name   = "${var.iam_user_policy_name}SQSPolicy"
+  user   = "${element(aws_iam_user.sqs_iam_user.*.name, count.index)}"
+  policy = "${data.aws_iam_policy_document.sqs_with_redrive_policy_document.json}"
+}
+
+resource "aws_iam_user_policy" "sqs_with_kms_and_redrive_user_policy" {
+  count = "${length(var.kms_alias) != 0 && length(var.redrive_arn) != 0 && length(var.policy) != 0 ? var.number_of_users : 0 }"
+
+  name   = "${var.iam_user_policy_name}SQSPolicy"
+  user   = "${element(aws_iam_user.sqs_with_kms_iam_user.*.name, count.index)}"
+  policy = "${data.aws_iam_policy_document.sqs_with_kms_and_redrive_policy_document.json}"
+}
+
 data "aws_iam_policy_document" "sqs_policy_document" {
-  count     = "${length(var.kms_alias) == 0 ? 1 : 0}"
+  count     = "${length(var.kms_alias) == 0 && length(var.redrive_arn) == 0 && length(var.policy) != 0 ? 1 : 0}"
   policy_id = "${var.sqs_iam_user}SQSPolicy"
 
   statement {
@@ -133,7 +255,7 @@ data "aws_iam_policy_document" "sqs_policy_document" {
 }
 
 data "aws_iam_policy_document" "sqs_with_kms_policy_document" {
-  count     = "${length(var.kms_alias) == 0 ? 0 : 1 }"
+  count     = "${length(var.kms_alias) != 0 && length(var.redrive_arn) == 0 && length(var.policy) != 0 ? 1 : 0 }"
   policy_id = "${var.sqs_iam_user}SQSPolicy"
 
   statement {
@@ -155,6 +277,67 @@ data "aws_iam_policy_document" "sqs_with_kms_policy_document" {
 
     resources = [
       "${aws_kms_key.sqs_kms_key.arn}",
+      "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:alias/${var.kms_alias}",
+    ]
+
+    actions = [
+      "kms:ReEncrypt",
+      "kms:GetKeyRotationStatus",
+      "kms:GetKeyPolicy",
+      "kms:GenerateRandom",
+      "kms:GenerateDataKeyWithoutPlaintext",
+      "kms:GenerateDataKey",
+      "kms:Encrypt",
+      "kms:DescribeKey",
+      "kms:Decrypt",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "sqs_with_redrive_policy_document" {
+  count     = "${length(var.kms_alias) == 0 && length(var.redrive_arn) != 0 && length(var.policy) != 0 ? 1 : 0}"
+  policy_id = "${var.sqs_iam_user}SQSPolicy"
+
+  statement {
+    sid    = "IAMSQSPermissions"
+    effect = "Allow"
+
+    resources = [
+      "${var.redrive_arn}",
+      "${aws_sqs_queue.queue_with_redrive.arn}",
+    ]
+
+    actions = [
+      "sqs:*",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "sqs_with_kms_and_redrive_policy_document" {
+  count     = "${length(var.kms_alias) != 0 && length(var.redrive_arn) != 0 && length(var.policy) != 0 ? 1 : 0 }"
+  policy_id = "${var.sqs_iam_user}SQSPolicy"
+
+  statement {
+    sid    = "IAMSQSPermissions"
+    effect = "Allow"
+
+    resources = [
+      "${var.redrive_arn}",
+      "${aws_sqs_queue.queue_with_kms_and_redrive.arn}",
+    ]
+
+    actions = [
+      "sqs:*",
+    ]
+  }
+
+  statement {
+    sid    = "KMSPermissions"
+    effect = "Allow"
+
+    resources = [
+      "${var.redrive_arn}",
+      "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:key/${var.kms_key}",
       "arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:alias/${var.kms_alias}",
     ]
 
