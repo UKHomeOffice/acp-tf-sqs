82746f0e732593c8f3184d431b18549de807f096
tasharnvb <tasharn.b@gmail.com>
2018-06-21 11:27:39 +0100

    Fix sqs outputs

--- output.tf
+++ output.tf
@@ -1,20 +1,19 @@
-#output "sqs_id" {
-#  description = "The SQS Queue ID we just created"
-#  value       = "${aws_sqs_queue.queue.id}"
-#}
-#
-#output "sqs_arn" {
-#  description = "The SQS Queue ARN we just created"
-#  value       = "${aws_sqs_queue.queue.arn}"
-#}
-#
-#output "sqs_id_kms" {
-#  description = "The SQS KMS Queue ID we just created"
-#  value       = "${aws_sqs_queue.queue_with_kms.id}"
-#}
-#
-#output "sqs_arn_kms" {
-#  description = "The SQS KMS Queue ARN we just created"
-#  value       = "${aws_sqs_queue.queue_with_kms.arn}"
-#}
+output "sqs_id" {
+ description = "The SQS Queue ID we just created"
+ value       = "${aws_sqs_queue.queue.*.id}"
+}
 
+output "sqs_arn" {
+ description = "The SQS Queue ARN we just created"
+ value       = "${aws_sqs_queue.queue.*.arn}"
+}
+
+output "sqs_id_kms" {
+ description = "The SQS KMS Queue ID we just created"
+ value       = "${aws_sqs_queue.queue_with_kms.*.id}"
+}
+
+output "sqs_arn_kms" {
+ description = "The SQS KMS Queue ARN we just created"
+ value       = "${aws_sqs_queue.queue_with_kms.*.arn}"
+}
