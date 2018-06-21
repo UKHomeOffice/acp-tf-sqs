8b7be09e8b1f0a3210aa68f25af3140dc73afd50
tasharnvb <tasharn.b@gmail.com>
2018-06-21 14:39:52 +0100

    Fix sqs output type

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
 output "sqs_id" {
- description = "The SQS Queue ID we just created"
- value       = "${aws_sqs_queue.queue.*.id}"
+  description = "The SQS Queue ID we just created"
+  value       = "${element(concat(aws_sqs_queue.queue.*.id, list("")), 0)}"
 }
 
 output "sqs_arn" {
- description = "The SQS Queue ARN we just created"
- value       = "${aws_sqs_queue.queue.*.arn}"
+  description = "The SQS Queue ARN we just created"
+  value       = "${element(concat(aws_sqs_queue.queue.*.arn, list("")), 0)}"
 }
 
 output "sqs_id_kms" {
- description = "The SQS KMS Queue ID we just created"
- value       = "${aws_sqs_queue.queue_with_kms.*.id}"
+  description = "The SQS KMS Queue ID we just created"
+  value       = "${element(concat(aws_sqs_queue.queue_with_kms.*.id, list("")), 0)}"
 }
 
 output "sqs_arn_kms" {
- description = "The SQS KMS Queue ARN we just created"
- value       = "${aws_sqs_queue.queue_with_kms.*.arn}"
+  description = "The SQS KMS Queue ARN we just created"
+  value       = "${element(concat(aws_sqs_queue.queue_with_kms.*.arn, list("")), 0)}"
 }
