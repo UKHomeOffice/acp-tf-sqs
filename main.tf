2d98d3b5b78184518f335a350b8df68d49d62811
samuel.walters <samuel.walters@digital.homeoffice.gov.uk>
2026-04-28 18:11:52 +0100

    ACPENG-3033 widen AWS provider constraint to >= 3.0, < 5.0

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.70"
+      version = ">= 3.0, < 5.0"
     }
   }
   required_version = ">= 1.0"
